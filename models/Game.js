d35a62ca04e82538ba2b0bdc0f47cdc23b047e57
Vladyslav Panchenko <vladlibero@gmail.com>
2020-01-14 23:14:41 +0200

    still working over models

--- models/Game.js
+++ models/Game.js
@@ -1,12 +1,11 @@
 const { Schema, model, Types } = require('mongoose')
 
 const schema = new Schema({
-   from: { type: String, required: true },
-   to: { type: String, required: true, unique: true },
-   code: { type: String, required: true, unique: true },
+   players: [{ type: Types.ObjectId, ref: 'User' }],
+   gameState: [{ type: String }], //TODO set default game state(state of game field)
    date: { type: Date, default: Date.now },
-   clicks: { type: Number, default: 0 },
-   owner: { type: Types.ObjectId, ref: 'User' }
+   inProgress: { type: Boolean, default: true },
+   winner: { type: String, default: 'DRAW' } //TODO do I use type: Types.ObjectId, ref: 'User' here?
 })
 
 module.exports = model('Game', schema)
--- models/User.js
+++ models/User.js
@@ -3,9 +3,10 @@ const { Schema, model, Types } = require('mongoose')
 const schema = new Schema({
    login: { type: String, required: true, unique: true },
    password: { type: String, required: true },
-   status: { type: String, required: true },
-   game: { type: String,  }
-//    links: [{ type: Types.ObjectId, ref: 'Link' }]
+   nickname: { type: String, default: 'NewPlayer' },
+   avatar: { type: String, default: '//default-ava-link' }, //TODO change default ava link
+   status: { type: String, default: 'READY' },  //READY PLAYING OFFLINE or READY PLAYING + keep online-offline in backend Redux?
+   currentGame: { type: Types.ObjectId, ref: 'Game' }
 })
 
 module.exports = model('User', schema)
